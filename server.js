5fa05aef8e63ac7a9a6a88ef1677c78d96f6a411
mishiba <58643640+mishiba@users.noreply.github.com>
2020-03-09 15:40:18 +0900

    Update server.js
    bug fix

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-var express = require('express'),
-    app     = express();
+var http = require('http);
+var server = http.createServer();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.use(express.static(__dirname + '/dist'));
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app;
+server.on('request', function(req, res) {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.write('hello world');
+    res.end();
+})
